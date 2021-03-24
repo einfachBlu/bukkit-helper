342f290c07aefe8c98c6cbaee85332ee21c2d69a
Bluplayz <bluplayz98@gmail.com>
2021-03-24 17:36:03 +0100

    removed debug listener

--- src/main/java/de/blu/bukkithelper/listener/TestListener.java
+++ src/main/java/de/blu/bukkithelper/listener/TestListener.java
@@ -2,9 +2,7 @@ package de.blu.bukkithelper.listener;
 
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
-import de.blu.bukkithelper.itembuilder.ItemBuilder;
 import de.blu.database.connection.redis.RedisConnection;
-import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -18,17 +16,5 @@ public final class TestListener implements Listener {
   @EventHandler
   public void onJoin(PlayerJoinEvent e) {
     Player player = e.getPlayer();
-    player.sendMessage("Redis connected: " + this.redisConnection.isConnected());
-    player.getInventory().clear();
-    player
-        .getInventory()
-        .addItem(
-            ItemBuilder.normal(Material.WATCH)
-                .setOnRightClickListener(
-                    e2 -> {
-                      e2.getPlayer().sendMessage("Yes!");
-                    },
-                    1000)
-                .build());
   }
 }
